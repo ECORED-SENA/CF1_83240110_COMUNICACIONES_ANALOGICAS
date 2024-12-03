18d5ae505dc0817e180b629e0e2b9542c49ce5e2
emilydev <emilse.a.b@gmail.com>
2024-12-03 07:44:55 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -197,10 +197,10 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Carlos Hernán Muñoz Carvajal',
+          nombre: 'Fabio Enrique Combariz',
           cargo: 'Experto Temático',
           centro:
-            'Centro de Teleinformática y Producción Industrial - Regional Cauca',
+            'Equipo de Adecuación Gráfica y Didáctica de Recursos Educativos - Regional Risaralda',
         },
         {
           nombre: 'Paola Alexandra Moya',
@@ -213,23 +213,13 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Carlos Julian Ramirez Benitez',
+          nombre: 'Juan Daniel Polanco',
           cargo: 'Diseñador de Contenidos Digitales',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
         {
           nombre: 'Edgar Mauricio Cortes Garcia',
           cargo: 'Desarrollador Fullstack',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
-        },
-        {
-          nombre: 'Daniela Muñoz Bedoya',
-          cargo: 'Animador y Productor Multimedia',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
-        },
-        {
-          nombre: 'Edgar Mauricio Cortes Garcia',
-          cargo: 'Actividad Didáctica',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
       ],
@@ -239,11 +229,6 @@ export default {
       autores: [
         {
           nombre: 'Luis Gabriel Urueta Alvarez',
-          cargo: 'Validador de Recursos Educativos Digitales',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
-        },
-        {
-          nombre: 'Jaime Hernán Tejada Llano',
           cargo: 'Validador de Recursos Educativos Digitales',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
